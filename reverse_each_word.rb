e9f006dba40fb47e5212d7f6c1cb30029f217857
Alicia Chao <aliciachao@gmail.com>
2019-12-10 06:04:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
 def reverse_each_word(string_sent)
-  arr = string_sent  # "12345"
-  arr.split('') # ["1","2","3","4","5"]
-  yield(string_sent.reverse)
-end
+# turn string into array of words
+# iterate over each word (index in array) and call reverse on each!
+# map to new array
 
-reverse_each_word(sentence1).collect {|n| n.reverse}
+  word_array = string_sent.split(' ')
+  # word_array.reverse => ["you?", "are", "how", "and", "there,", "Hello"]
+
+  result = word_array.map {|word| word.reverse}
+  result.join(" ")
+end 
